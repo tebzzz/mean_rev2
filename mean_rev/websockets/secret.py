5377cbf0b05dd3c79758e0a5b2e263fca10b5224
tebzzz <tebzzz@gmail.com>
2023-01-23 00:24:26 -0700

    Delete mean_rev directory

--- mean_rev/websockets/secret.py
+++
@@ -1,5 +0,0 @@
-username = 'postgres'  # DB username
-password = 'proddb123'  # DB password
-host = '34.84.190.250'  # Public IP address for your instance
-port = '5432'
-database = 'prod'  # Name of database ('postgres' by default)
